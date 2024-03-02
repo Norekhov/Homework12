112df7f7416836c35b479718c135b515481720a9
Никита <nikit.orekhoff@yandex.ru>
2024-03-02 19:28:27 +0300

    Домашняя работа № 13

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private int yearRelease;
@@ -8,6 +10,25 @@ public class Book {
         this.yearRelease = yearRelease;
         this.author = author;
     }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + title + ", год издания - " + yearRelease + ", автор - " + author + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+
 
     public String getTitle() {
         return this.title;
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Author tolstoy = new Author("Лев", "Толстой");
         Book one = new Book("Колобок", 1910, tolstoy);
-        System.out.println("Название книги - " + one.getTitle() + ", год выпуска - " + one.getYearRelease() + ". Автор книги - " + one.getAuthor().getName() + " " + one.getAuthor().getSurname() + ".");
-
+        System.out.println(one);
 
         Author pushkin = new Author("Александр", "Пушкин");
         Book two = new Book("Евгений Онегин", 1833, pushkin);
-        System.out.println("Название книги - " + two.getTitle() + ", год выпуска - " + two.getYearRelease() + ". Автор книги - " + two.getAuthor().getName() + " " + two.getAuthor().getSurname() + ".");
+        System.out.println(two);
 
 
         Author lermontov = new Author("Михаил", "Лермонтов");
         Book three = new Book("Герой нашего времени", 1838, lermontov);
-        System.out.println("Название книги - " + three.getTitle() + ", год выпуска - " + three.getYearRelease() + ". Автор книги - " + three.getAuthor().getName() + " " + three.getAuthor().getSurname() + ".");
+        System.out.println(three);
         three.setYearRelease(1846);
-        System.out.println("Название книги - " + three.getTitle() + ", год выпуска - " + three.getYearRelease() + ". Автор книги - " + three.getAuthor().getName() + " " + three.getAuthor().getSurname() + ".");
+        System.out.println(three);
     }
 }
